a9596cfb07cadd81e955f45099c178375be8ac77
tingjianlau <tingjianlau@gmail.com>
2016-05-16 11:40:53 +0800

    add 70

---
+++ 70_climbing_staris/70_climbing_staris.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+##########################################################################
+#	> File Name: 70_climbing_staris.py
+#	> Author: Tingjian Lau
+#	> Mail: tjliu@mail.ustc.edu.cn
+#	> Created Time: 2016/05/16
+#########################################################################
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 3: return n
+        
+        first, second, ways = 2, 3, 0
+        
+        for i in range(3, n):
+            ways = first + second
+            first = second
+            second = ways
+            
+        return ways
---
+++ 70_climbing_staris/README.md
@@ -0,0 +1,10 @@
+##Link (fibonacci数列的非递归写法)
+https://leetcode.com/problems/climbing-stairs/
+
+##Details
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+##Tags
+Dynamic Programming
